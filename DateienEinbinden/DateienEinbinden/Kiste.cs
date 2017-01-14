ecabe74f7feef789ce26bb1e8ff2d93749187aa1
Jan Suchotzki <jan@lernmoment.de>
2017-01-14 16:04:47 +0100

    Fügt Quellcode für Text/XML-Datei einbinden hinzu

---
+++ DateienEinbinden/DateienEinbinden/Kiste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateienEinbinden
+{
+    public enum Zimmer
+    {
+        Wohnzimmer,
+        Küche,
+        Kinderzimmer,
+        Schlafzimmer,
+        Bad,
+        Keller
+    }
+
+    class Kiste
+    {
+        public Kiste(int nummer)
+        {
+            Nummer = nummer;
+        }
+
+        public int Nummer { get; private set; }
+        public string Inhalt { get; set; }
+        public Zimmer ZielOrt { get; set; }
+
+        public override string ToString()
+        {
+            return "KISTE: " + Nummer + ", " + ZielOrt + ", " + Inhalt;
+        }
+    }
+}
---
+++ DateienEinbinden/DateienEinbinden/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateienEinbinden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateiName = "Testdaten/KistenVorrat.txt";
+            string[] eingeleseneKistenBeschreibung;
+            IEnumerable<Kiste> kisten;
+
+            eingeleseneKistenBeschreibung = KistenEinlesenAusDatei(dateiName);
+            kisten = KistenErstellen(eingeleseneKistenBeschreibung);
+
+            foreach (Kiste kiste in kisten)
+            {
+                Console.WriteLine(kiste);
+            }
+
+            Console.ReadLine();
+        }
+
+        static private string[] KistenEinlesenAusDatei(string dateiName)
+        {
+            string[] kistenAusDatei = null;
+
+            try
+            {
+                kistenAusDatei = File.ReadAllLines(dateiName, Encoding.Default);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+                throw e;
+            }
+
+            return kistenAusDatei;
+        }
+
+        static private IEnumerable<Kiste> KistenErstellen(string[] kistenBeschreibungen)
+        {
+            List<Kiste> kisten = new List<Kiste>();
+
+            foreach (string beschreibung in kistenBeschreibungen)
+            {
+                string[] eigenschaften = beschreibung.Split(',');
+                Kiste neueKiste = new Kiste(int.Parse(eigenschaften[0]));
+                neueKiste.ZielOrt = (Zimmer) Enum.Parse(typeof(Zimmer), eigenschaften[1]);
+                neueKiste.Inhalt = eigenschaften[2];
+
+                kisten.Add(neueKiste);
+            }
+
+            return kisten;
+        }
+    }
+}
